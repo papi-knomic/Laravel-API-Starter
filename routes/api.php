5d9bfb2ca8f10309236973cffbd9c5ff8c7bf069
Samson Moses <knomic10@gmail.com>
2023-02-06 12:00:37 +0100

    Added Response trait, ForceJson middleware and create root route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Traits\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => ['json']], function () {
+    Route::get('/', function () {
+        return Response::successResponse('Welcome to FOSI');
+    });
 });
