580f3ba4069a1e4c9661035d26ce9ed0a499f308
Samson Moses <knomic10@gmail.com>
2023-02-12 12:52:41 +0100

    Finished login function, works well now

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
 use App\Repositories\UserRepository;
 use App\Traits\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,6 +29,19 @@ class AuthController extends Controller
         $user = $this->userRepository->create($fields);
 
         return Response::successResponseWithData($user, 'Successful!, check your mail for verification code');
+    }
+
+    public function login( LoginUserRequest $request ) : JsonResponse
+    {
+        $userData = $request->validated();
+
+        if (Auth::attempt($userData)) {
+            $token = config('keys.token');
+            $accessToken = Auth::user()->createToken($token)->plainTextToken;
+            $data = auth()->user();
+            return Response::successResponseWithData($data, 'Login successful', 200, $accessToken);
+        }
+        return Response::errorResponse('Invalid Login credentials', 400);
     }
 
 }
--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginUserRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class LoginUserRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,8 +24,7 @@ class LoginUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|email|exists:users,email',
-            'password' => 'required|string',
+            //
         ];
     }
 }
