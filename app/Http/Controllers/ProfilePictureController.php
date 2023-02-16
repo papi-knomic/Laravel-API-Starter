d5abcd0211bf04e79c145d7feca34165f1c84391
Samson Moses <knomic10@gmail.com>
2023-02-16 12:36:36 +0100

    Added profile picture table and turned the column on users to a foreign key

---
+++ app/Http/Controllers/ProfilePictureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProfilePicture;
+use Illuminate\Http\Request;
+
+class ProfilePictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProfilePicture  $profilePicture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProfilePicture $profilePicture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProfilePicture  $profilePicture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProfilePicture $profilePicture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProfilePicture  $profilePicture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProfilePicture $profilePicture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProfilePicture  $profilePicture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProfilePicture $profilePicture)
+    {
+        //
+    }
+}
